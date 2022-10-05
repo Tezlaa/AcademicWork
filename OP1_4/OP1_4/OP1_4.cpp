e3b00935fa4d633f4dca0e847ea327ab15445cac
Bogdan <95874565+Tezla1992@users.noreply.github.com>
2022-10-05 19:05:10 +0200

    OP1_4

---
+++ OP1_4/OP1_4/OP1_4.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    float visco;
+    string result;
+
+    cout << "machine lubrication" << endl << endl;
+
+    cout << "Write your viscosity: "; cin >> visco;
+
+    result = "Error your viscosity <3.50";
+
+    if (3.5 <= visco && visco <= 9)
+    {
+        result = "light viscosity";
+    }
+    if (10 <= visco && visco <= 57)
+    {
+        result = "average viscosity";
+    }
+    if (visco >= 58)
+    {
+        result = "heavy";
+    }
+
+    cout << result << endl << endl;
+
+    return 0;
+}
