974314845ffec89835b7c4ba373d3ef1344e5b92
Bogdan <95874565+Tezla1992@users.noreply.github.com>
2022-10-11 23:59:51 +0200

    OP1_6

---
+++ OP1_6/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+
+    int n, k, i, d;
+    float f;
+
+    cout << "Write n,k 0<=k<=n" << endl;
+    cin >> n; cin >> k;
+
+    //k!
+    f = 1;
+    for (i = 1; i <= k; i++)
+    {
+        f = f * i;
+    }
+
+    i = 0;
+    d = 1;
+    do
+    {
+        i++;
+        d = d * (n * (n - i));
+        
+    } while (i < k);
+
+    cout << endl << d / f << endl;
+    return 0;
+}
