0cd319dc1bbcc249654e1d254d789ecaa60d9a50
Bogdan <95874565+Tezla1992@users.noreply.github.com>
2022-10-12 22:37:54 +0200

    OP1_7

---
+++ OP1_7/OP1_7/OP1_7.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    
+    float e, x, u1, u2, result, n;
+
+    cout << "Write X: "; cin >> x;
+    cout << "Write E: "; cin >> e;
+
+    u1 = 0;
+    u2 = 1;
+    result = 0;
+    n = 0;
+
+    while (abs(u2 - u1) >= e)
+    {   
+        u1 = u2;
+        n = n + 2;
+        u2 = u1 + (pow(x, n) / n);
+        result = u2;
+    }
+
+    cout << "N - " << n/2 << endl << "R - " << result << endl;
+
+}
