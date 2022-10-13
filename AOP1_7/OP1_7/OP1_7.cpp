ceac305dd11b4112fdd1fe5a144b7cf27ea9ee54
Bogdan <95874565+Tezla1992@users.noreply.github.com>
2022-10-13 20:57:14 +0200

    AOP1_7

---
+++ AOP1_7/OP1_7/OP1_7.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    float e, x, u, u2, n;
+
+    cout << "Write X: "; cin >> x;
+    cout << "Write E: "; cin >> e;
+
+    u = x;
+    u2 = 1;
+    n = 0;
+
+    while (abs(u) >= e)
+    {   
+        u = u2 * (x / (n+1));
+        n = n + 2;
+        u2 = u;
+        cout << "n: " << n << endl << "U: " << u << endl << "U2: " << u2 << endl;
+
+    }
+
+    cout << "n: " << n/2 << endl << "U: " << u << endl;
+}
