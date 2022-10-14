336c0f1f9ee6c8cd6720bd1285b388342336d21f
Bogdan <95874565+Tezla1992@users.noreply.github.com>
2022-10-14 22:18:25 +0200

    update14.10

--- AOP1_7/OP1_7/OP1_7.cpp
+++ AOP1_7/OP1_7/OP1_7.cpp
@@ -3,23 +3,26 @@ using namespace std;
 
 int main()
 {
-    float e, x, u, u2, n;
+    float e, x, u1, u2, n, sum;
 
     cout << "Write X: "; cin >> x;
     cout << "Write E: "; cin >> e;
 
-    u = x;
-    u2 = 1;
-    n = 0;
+    u1 = 1;
+    n = 1;
+    sum = 1;
+    u2 = 0;
 
-    while (abs(u) >= e)
+    while (abs(u1) >= e)
     {   
-        u = u2 * (x / (n+1));
-        n = n + 2;
-        u2 = u;
-        cout << "n: " << n << endl << "U: " << u << endl << "U2: " << u2 << endl;
+        u2 = u1 * (x*x) / ((2*n-1)*(2*n));
+        sum += u2;
+        u1 = u2;
+        n = n + 1;
+        cout << "u2: " << u2 << endl << "u1: " << u2 << endl << "n: " << n << endl << endl;
 
     }
 
-    cout << "n: " << n/2 << endl << "U: " << u << endl;
+    cout << "N: " << n << endl << "U: " << u1 << endl;
+    return 0;
 }
