adc59ef5ab758bfbe0b32747584ae5d772d02c5f
Bogdan <95874565+Tezla1992@users.noreply.github.com>
2022-10-18 20:09:13 +0200

    18.10.22

---
+++ AOP1_8/AOP1_8/AOP1_8.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int t, age;
+	float result;
+	const int n = 10;
+
+	cin >> age;
+	int* m = new int[age];
+
+	for (t = 1; t <= age; t++)
+	{
+		m[t] = 0 + rand() % 1000;
+
+		cout << m[t] << "\t";
+
+		result = m[t] / (n * t);
+
+		cout << "AGE " << t << " - " << result << " Off\n";
+	}
+
+
+
+	return 0;
+}
