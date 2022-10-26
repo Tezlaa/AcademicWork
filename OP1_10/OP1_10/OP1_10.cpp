980442cd474670dbe1b8270cc7d06eff4784c5fc
Bogdan <95874565+Tezla1992@users.noreply.github.com>
2022-10-26 21:11:35 +0200

    26.10.22/21:10

--- OP1_10/OP1_10/OP1_10.cpp
+++ OP1_10/OP1_10/OP1_10.cpp
@@ -1,7 +1,40 @@
 ﻿#include <iostream>
+
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
+    int N, k;
+
+    cout << "Write your matrix:"; cin >> N;
+
+    int* A = new int[N];
+
+    cout << "Matrix:";
+
+    for (int i = 0; i < N; i++)
+    {
+        A[i] = 2 * rand() % 40;
+        cout << A[i] << " ";
+    }
+
+    cout << "\nWrite index: ";
+    cin >> k;
+
+    for (int i = k; i < N - 1; i++) //Двигаемся слева направо с заданной позиции
+    {
+        A[i] = A[i + 1]; //Сдвигаем элементы
+    }
+
+   
+    cout << "\nFinish matrix:";
+
+    for (int i = 0; i < N - 1; i++) //Уменьшаем размер массива на единицу
+    {
+        cout << A[i] << " "; //Выводим массив после удаления элемента
+    }
+
+    cout << endl;
+    delete[] A;
+    return 0;
 }
