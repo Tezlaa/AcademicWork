64044dc45d0ed3ff4866aecaec797932e9f8ac5c
Bogdan <95874565+Tezla1992@users.noreply.github.com>
2022-10-28 02:39:41 +0200

    28.10.22/2:39

--- OP1_11(2)/OP1_11(2)/OP1_11(2).cpp
+++ OP1_11(2)/OP1_11(2)/OP1_11(2).cpp
@@ -1,6 +1,4 @@
-﻿
-
-#include <iostream>
+﻿#include <iostream>
 #include <iomanip>
 using namespace std;
 
@@ -9,19 +7,18 @@ int main()
     int m, n, i, j;
     float sumn, tamp;
 
-    setlocale(LC_ALL, "Russian");
-    cout << "Введите количество рядков: "; cin >> m;
-    cout << "Введите количество столбцов: "; cin >> n;
+    cout << "Enter the number of rows: "; cin >> n;
+    cout << "Enter the number of columns: "; cin >> m;
 
-    float** a = new float* [m];
-    float* sum = new float[m];
+    float** a = new float* [n];
+    float* sum = new float[n];
 
-    for (i = 0; i < m; i++)
-        a[i] = new float[n];
+    for (i = 0; i < n; i++)
+        a[i] = new float[m];
 
-    for (i = 0; i < m; i++)
+    for (i = 0; i < n; i++)
     {
-        for (j = 0; j < n; j++)
+        for (j = 0; j < m; j++)
         {
             a[i][j] = -10 + rand() % 21;
             cout << setw(5) << a[i][j];
@@ -29,17 +26,19 @@ int main()
         cout << endl;
     }
 
-    for (j = 0; j < n; j++)
+    //sum columns
+    for (j = 0; j < m; j++)
     {
         sumn = 0;
-        for (i = 0; i < m; i++)
+        for (i = 0; i < n; i++)
         {
             sumn += a[i][j];
         }
         sum[j] = sumn;
     }
 
-    for (j = 0; j < n; j++)
+    //sorted sum and main matrix
+    for (j = 0; j < m; j++)
     {
         if (sum[j] < sum[j + 1])
         {
@@ -47,7 +46,7 @@ int main()
             sum[j] = sum[j + 1];
             sum[j + 1] = tamp;
 
-            for (i = 0; i < m ; i++)
+            for (i = 0; i < n ; i++)
             {
                 tamp = a[i][j];
                 a[i][j] = a[i][j + 1];
@@ -57,21 +56,23 @@ int main()
         }
     }
 
-    for (j = 0; j < m; j++)
+    //sum display
+    for (j = 0; j < n; j++)
     {
         cout << sum[j] << " ";
     }
+
     cout << endl;
 
-    for (i = 0; i < m; i++)
+    //display sorted matrix
+    for (i = 0; i < n; i++)
     {
-        for (j = 0; j < n; j++)
+        for (j = 0; j < m; j++)
         {
             cout << setw(5) << a[i][j];
         }
         cout << endl;
     }
 
-    delete[]a; delete[]sum;
     return 0;
 }
