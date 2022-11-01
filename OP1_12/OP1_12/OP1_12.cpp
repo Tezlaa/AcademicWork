40d47f7294e21beb943910ac12d45168ff6cfe91
Bogdan <95874565+Tezla1992@users.noreply.github.com>
2022-11-01 23:41:49 +0200

    01.11.22/23:41

---
+++ OP1_12/OP1_12/OP1_12.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+float result, e;
+
+float formula(float, float, float);
+
+int main()
+{
+    float z, x, y;
+    
+    cout << "Write X: "; cin >> x; cout << endl;
+    cout << "Write Y: "; cin >> y; cout << endl;
+    cout << "Write E: "; cin >> e; cout << endl;
+
+    z = formula(-2, x, -3) / abs(y + formula(x, y, 0)) - sin(x + y) + formula (1, -y, 0);
+
+    cout << z;
+
+    return 0;
+}
+
+float formula(float a, float b, float p)
+{
+    result = pow(e, -a * b) - p;
+    cout << endl << result << endl;
+    return result;
+}
