9b1f51813f16de7464ea2d828930a2a5b27d913c
Bogdan <95874565+Tezla1992@users.noreply.github.com>
2022-11-08 22:49:54 +0200

    08.11.22/22:49

---
+++ OP1_13(1)/OP1_13(1)/OP1_13(1).cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, i;
+
+    cout << "Write length array: "; cin >> n;
+
+    float *array = new float[n];
+
+    for (i = 0; i < n; i++)
+    {
+        array[i] = (0.2 + rand() % 15) / 3;
+        cout << array[i] << " ";
+    }
+
+    return 0;
+}
+
+float func(float *a, int length)
+{
+    float min;
+
+    min = a[0];
+
+    for (int i = 1; i = length; i++)
+    {
+        -
+    }
+}
