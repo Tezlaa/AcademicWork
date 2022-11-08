51b164a0835cba6fd594cc1e9537688cd7f59bc8
Bogdan <95874565+Tezla1992@users.noreply.github.com>
2022-11-09 00:51:32 +0200

    09.11.22/0:51

--- OP1_13(2)/OP1_13(2)/OP1_13(2).cpp
+++ OP1_13(2)/OP1_13(2)/OP1_13(2).cpp
@@ -1,7 +1,37 @@
 ﻿#include <iostream>
 using namespace std;
 
+int ready(int* , int);
 int main()
 {
-    cout << "Hello World!\n";
+    int n, i, t;
+
+    cout << "Write quantities: "; cin >> n;
+
+    int* array = new int[n];
+
+    for (i=0; i < n; i++)
+    {
+        *(array + i) = rand() % 26;
+        cout << *(array + i) << " ";
+    }
+
+    cout << "\n\n\n" << ready(array, n) << endl;
+
+    return 0;
+}
+
+int ready(int* arr, int length)
+{
+    int ready = 0;
+
+    for (int i = 0; i < length; i++)
+    {
+        if (*(arr + i) >= 20)
+        {
+            ready += 1;
+        }
+    }
+
+    return ready;
 }
