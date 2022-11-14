c60b5e69a64a96d0d74bd4ac885af74268c9fa51
Bogdan <95874565+Tezla1992@users.noreply.github.com>
2022-11-14 02:14:19 +0200

    14/11/2022/02:14

--- OP1_14(1)/OP1_14(1)/OP1_14(1).cpp
+++ OP1_14(1)/OP1_14(1)/OP1_14(1).cpp
@@ -1,7 +1,39 @@
 ﻿#include <iostream>
+#include <string>
 using namespace std;
+
+string print_in_column(string);
+
+string print_in_column(string self_text) //creating a funcion for separation on 3 colums
+{
+    int counter = 0;  //index for separation on 3 colums
+    string result_text;  //for the record result text
+
+    for (int i = self_text.size() - 1; i >= 0; i--) // function which begins at the end of
+    {
+        if (counter == 3) // check if the number of columns is 3
+        {
+            result_text = result_text + "\n"; // if the columns are equal to 3, then go to the new line
+            counter = 0; //zeroing
+        }
+        else
+        {
+            result_text = result_text + self_text[i] + " "; //add symbol by index to the result text
+            counter += 1; // plus 1 to index
+        }
+    }
+    return result_text;
+}
 
 int main()
 {
-    cout << "Hello World!\n";
+    string text, result;
+
+    cout << "Write your characters via spaces: "; getline(cin, text);
+    
+    result = print_in_column(text);
+
+    cout << print_in_column(text);
+
+    return 0;
 }
