24cac83fe126c92c7e1b35e74797f03deab35e83
Bogdan <95874565+Tezla1992@users.noreply.github.com>
2022-11-17 16:26:10 +0200

    17\11\2022\16:26

--- OP1_14(1)/OP1_14(1)/OP1_14(1).cpp
+++ OP1_14(1)/OP1_14(1)/OP1_14(1).cpp
@@ -9,31 +9,25 @@ string print_in_column(string self_text) //creating a funcion for separation on 
     int counter = 0;  //index for separation on 3 colums
     string result_text ="";  //for the record result text
 
-    int j = 2; // counter for reversing 3 numbers
-
-    for (int i = 0; i < self_text.size(); i++) // function which begins at the end of
+    for (int i = self_text.size(); i >= 0; i--) // function which begins at the end of
     {
         if (counter == 3) // check if the number of columns is 3
         {
-            result_text = result_text + "\n"; // if the columns are equal to 3, then go to the new line
+            result_text = result_text + " "; // if the columns are equal to 3, then add space
             counter = 0; //zeroing
-            i--; //go back to the previous index
+            i++; //go back to the previous index
         }
         else
         {
             if (self_text[i] == '\0') //if the last char
             {
-                result_text = result_text + self_text[j - i + 1] + " "; //if this symbol is the first add next char
-                i++; //not to go out of range
-            }
-            else
-            {
-                result_text = result_text + self_text[j - i] + " "; //add symbol by index to the result text
+                result_text = result_text + self_text[i - 1]; //if this symbol is the first add next char
+                i--; //not to go out of range
             }
 
-            if (counter + 1 == 3) //check if next counter == 3 , then counter plus 6, to get sequence from right to left
+            else
             {
-                j += 6;
+                result_text = result_text + self_text[i]; //add symbol by index to the result text
             }
 
             counter += 1; //plus 1 to index
