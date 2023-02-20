b59f8fdd7846282a1d0d3a5131247afde7593d66
Tezla <Bogdantihenko60@gmail.com>
2023-02-20 21:11:42 +0200

    op2-1 20/02/23

--- Semester2/OP2_1/OP2_1/OP2_1/OP2_1.cpp
+++ Semester2/OP2_1/OP2_1/OP2_1/OP2_1.cpp
@@ -5,7 +5,9 @@ using namespace std;
 template <typename T>
 T max_element_on_diagonal(T** matrix, int rows, int cols)
 {
-    int size = rows < cols ? rows : cols;
+    // function for search max element on diagonal
+    int size = rows < cols ? rows : cols; // if rows < cols then size = rows else size = cols 
+    
     T max_element = matrix[0][0];
     for (int i = 1; i < size; i++)
     {
@@ -20,7 +22,9 @@ T max_element_on_diagonal(T** matrix, int rows, int cols)
 template <typename T>
 T min_element_on_diagonal(T** matrix, int rows, int cols)
 {
-    int size = rows < cols ? rows : cols;
+    // function for search min element on diagonal
+    int size = rows < cols ? rows : cols;  // if rows < cols then size = rows else size = cols 
+
     T min_element = matrix[0][0];
     for (int i = 1; i < size; i++)
     {
@@ -34,7 +38,8 @@ T min_element_on_diagonal(T** matrix, int rows, int cols)
 
 template <typename T>
 void sort_rows_in_ascending_order(T** matrix, int rows, int cols)
-{
+{   
+    // sort rows
     for (int i = 0; i < rows; i++)
     {
         for (int j = 0; j < cols - 1; j++)
@@ -55,6 +60,7 @@ void sort_rows_in_ascending_order(T** matrix, int rows, int cols)
 template <typename T>
 void sort_columns_in_descending_order(T** matrix, int rows, int cols)
 {
+    // sort columns
     for (int i = 0; i < cols; i++)
     {
         for (int j = 0; j < rows - 1; j++)
@@ -73,7 +79,9 @@ void sort_columns_in_descending_order(T** matrix, int rows, int cols)
 }
 
 template <typename T>
-void printMatrix(T** matrix, int rows, int cols) {
+void printMatrix(T** matrix, int rows, int cols)
+{
+    // function that shows the matrix
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
             cout << setw(8) << matrix[i][j];
@@ -82,22 +90,23 @@ void printMatrix(T** matrix, int rows, int cols) {
     }
 }
 
-
 template<typename T>
-T** create_random_matrix(int rows, int columns)
+T** create_matrix(int rows, int columns, int way)
 {
-    int counstant;
+    // way: 1- random matrix, 2- input matrix
+    // function creating input matrix
 
+    int counstant;
     int type_T = sizeof(T);
     switch (type_T)
     {
-    case 1:  // if char
+    case 1:  // if type T is char
         counstant = 1;
         break;
-    case 4:  // if int
+    case 4:  // if type T is int
         counstant = 1000;
         break;
-    case 8:  //if double
+    case 8:  // if type T is double
         counstant = 1000;
         break;
     default:
@@ -105,25 +114,36 @@ T** create_random_matrix(int rows, int columns)
         break;
     }
 
-    srand(time(nullptr));
+    // create matrix
     T** matrix = new T * [rows];
     for (int i = 0; i < rows; ++i) {
         matrix[i] = new T[columns];
     }
 
     for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < columns; ++j) {
-            matrix[i][j] = static_cast<T>(rand()) / counstant;
+        for (int j = 0; j < columns; ++j)
+        {
+            if (way == 1)   // if way_writing selected 1 (generation)
+            {
+                matrix[i][j] = static_cast<T>(rand()) / counstant;
+            }
+            else if (way == 2)   // if way_writing selected 2 (writing)
+            {
+                cout << i << ", " << j << " - "; cin >> matrix[i][j];
+            }
         }
     }
-
     return matrix;
 }
 
 template <typename T>
 void result_info(T** matrix, int row, int colm)
 {
-    cout << "\nMax element on diagonal: " << max_element_on_diagonal(matrix, row, colm) << endl;
+    cout << "Your matrix: \n";
+    printMatrix(matrix, row, colm);
+    
+    cout << "\n---------------------------------------";
+    cout << "\n\nMax element on diagonal: " << max_element_on_diagonal(matrix, row, colm) << endl;
     cout << "Min element on diagonal: " << min_element_on_diagonal(matrix, row, colm) << endl;
 
     cout << "\nSorted rows in ascending order: " << endl;
@@ -139,41 +159,45 @@ void result_info(T** matrix, int row, int colm)
 
 int main()
 {
-    /*
-    * printMatrix - function that shows the matrix
-    * result_info - shows all necessary parameters.
-                    Max/min element into matrix and
-                    sort element into matrix descending/ascending
-      
+    /* 
+    rows - row matrix,
+    cols - columns matrix, 
+    matrix_type - type matix(char, int, double),
+    way_writing - writing type(hand write or random matrix)
     */
-    
-    int ROWS, COLUMS;
-    cout << "Rows matrix: "; cin >> ROWS;
-    cout << "Colums matrix: "; cin >> COLUMS;
+    int rows, cols, matrix_type, way_writing;
 
-    //int example
-    int** int_matrix = create_random_matrix<int>(ROWS, COLUMS);
+    while (true)
+    {
+        cout << "Enter the number of rows in the matrix: "; cin >> rows;
+        cout << "Enter the number of columns in the matrix: "; cin >> cols;
+        cout << "\nEnter the type of the matrix:\n1 - char\n2 - int\n3 - double:   >>> "; cin >> matrix_type;
+        cout << "\nEnter the way writing matrix:\n1 - generation\n2 - input:\t>>> "; cin >> way_writing;
 
-    cout << "--------------------------------------";
-    cout << "\n\nStart random matrix INTEGER:" << endl;
-    printMatrix(int_matrix, ROWS, COLUMS);
-    result_info(int_matrix, ROWS, COLUMS);
-
-    //double example
-    double** double_matrix = create_random_matrix<double>(ROWS, COLUMS);
-
-    cout << "--------------------------------------";
-    cout << "\n\nStart random matrix DOUBLE:" << endl;
-    printMatrix(double_matrix, ROWS, COLUMS);
-    result_info(double_matrix, ROWS, COLUMS);
-
-    //char example
-    char** char_matrix = create_random_matrix<char>(ROWS, COLUMS);
-
-    cout << "--------------------------------------";
-    cout << "\n\nStart random matrix CHAR:" << endl;
-    printMatrix(char_matrix, ROWS, COLUMS);
-    result_info(char_matrix, ROWS, COLUMS);
-
+        if (matrix_type == 1)  // if char
+        {
+            char** matrix = create_matrix<char>(rows, cols, way_writing);
+            result_info(matrix, rows, cols);
+        }
+        else if (matrix_type == 2)  // if int
+        {
+            int** matrix = create_matrix<int>(rows, cols, way_writing);
+            result_info(matrix, rows, cols);
+        }
+        else if (matrix_type == 3)  // in double
+        {
+            double** matrix = create_matrix<double>(rows, cols, way_writing);
+            result_info(matrix, rows, cols);
+        }
+        else // if wrong
+        {
+            cout << "Invalid matrix type." << endl;
+            break;
+        }
+        
+        // pause and clear console for reboot
+        system("PAUSE");
+        system("cls");
+    }
     return 0;
 }
