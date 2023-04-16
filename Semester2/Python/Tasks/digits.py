6ebb9083c558ab7d2b0250fc96dcf57af1332dad
Tezla <Bogdantihenko60@gmail.com>
2023-04-17 01:39:37 +0300

    update

--- Semester2/Python/Tasks/digits.py
+++ Semester2/Python/Tasks/digits.py
@@ -6,27 +6,28 @@ Student = namedtuple('Student',
                      'initials score_math score_physics score_literature')
 
 
-def get_value(element) -> float:
-    """_summary_
+def _get_value(element: Dict[str, float]) -> float:
+    """Function to get the value in the sort method
+    This function calls only the 'key' argument
 
     Args:
-        element (_type_): _description_
+        element (Dict[str, float]): dict with name and avarage score
 
     Returns:
-        float: _description_
+        float: return avarage score
     """
     return list(element.values())[0]
 
 
 def select_by_passing_score(ranking: List[Dict[str, float]], score: float) -> List[Dict[str, float]]:
-    """_summary_
+    """Function to search for students with available avarage score only
 
     Args:
-        ranking (List[Dict[str, float]]): _description_
-        score (float): _description_
+        ranking (List[Dict[str, float]]): list with all ranking students
+        score (float): What scort to write on the result list
 
     Returns:
-        List[Dict[str, float]]: _description_
+        List[Dict[str, float]]: list with all students who have more score
     """
     
     result = []
@@ -38,13 +39,13 @@ def select_by_passing_score(ranking: List[Dict[str, float]], score: float) -> Li
 
 
 def ranking_students(students: List[Student]) -> List[Dict[str, float]]:
-    """_summary_
+    """Ranking student, take all students, give a sorted ranking list with all students
 
     Args:
-        students (List[Student]): _description_
+        students (List[Student]): all student
 
     Returns:
-        List[Dict[str, float]]: _description_
+        List[Dict[str, float]]: sorted list by avarage score
     """
     
     ranking = []
@@ -53,21 +54,25 @@ def ranking_students(students: List[Student]) -> List[Dict[str, float]]:
         quantity_object = 0
         
         for field in range(len(student)):
+            # search by name with a score_ to add this score
             if student._fields[field].find('score_') != -1:
                 amount_score += student[field]
                 quantity_object += 1
-        
+                
+        # add in list student initials and him avarage score by all search score
+        # example list: [{'NAME': AVARAGE SCORE}]
         ranking.append({student.initials: float('%.2f' % (amount_score / quantity_object))})
 
-    ranking.sort(key=get_value, reverse=True)
+    ranking.sort(key=_get_value, reverse=True)
     return ranking
 
 
 def filling_student_info() -> List[Student]:
-    """_summary_
+    """filling all list with students
 
     Returns:
-        List[Student]: _description_
+        List[Student]: list with all student
+            Example: [Student('NAME', 10, 9, 4, 5), ...]
     """
     
     students = []
@@ -75,14 +80,14 @@ def filling_student_info() -> List[Student]:
     while True:
         students.append(
             Student(
-                initials=input('Write the initials of the student: '),
-                score_math=int(input('Him score in math: ')),
-                score_physics=int(input('Him score in physics: ')),
-                score_literature=int(input('Him score in litetature: ')),
-            )
+                initials=input('\nWrite the initials of the student: '),
+                score_math=float(input('Him score in math: ')),
+                score_physics=float(input('Him score in physics: ')),
+                score_literature=float(input('Him score in litetature: ')),
+            ),
         )
         
-        select_path = input('Add next student?(y-Yes, n-No): ').lower()
+        select_path = input('\nAdd next student?(y-Yes, n-No): ').lower()
         if select_path != 'y':
             return students
         
@@ -90,7 +95,7 @@ def filling_student_info() -> List[Student]:
 if __name__ == "__main__":
     students = filling_student_info()
     ranking = ranking_students(students)
-    passing_ranking = select_by_passing_score(ranking, float(input('Write passing score: ')))
+    passing_ranking = select_by_passing_score(ranking, float(input('\nWrite passing score: ')))
     
     print(
         f'\nBest score average: {list(ranking[0].keys())[0]} - {list(ranking[0].values())[0]}\n'
@@ -98,5 +103,3 @@ if __name__ == "__main__":
     )
     for student in passing_ranking:
         print(f'{list(student.keys())[0]} - {list(student.values())[0]}')
-        
-    print()
