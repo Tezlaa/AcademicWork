10747878a7087122389c00eaabec1f9e63eef00b
Tezla <Bogdantihenko60@gmail.com>
2023-05-24 20:55:16 +0300

    create new page gallery

--- Semester2/Web/TrevCH/static/js/script.js
+++ Semester2/Web/TrevCH/static/js/script.js
@@ -1,10 +1,10 @@
 function changeImage() {
-    var close = 'static/images/background/select_menu.png';
-    var open = 'static/images/background/select_menu_close.png';
+    let close = 'static/images/background/select_menu.png';
+    let open = 'static/images/background/select_menu_close.png';
     
-    var navMenu = document.getElementById('navMenu')
-    var image = document.getElementById("myImage");
-    var availableSrc = 'static/' + image.src.split('static/')[1]
+    let navMenu = document.getElementById('navMenu')
+    let image = document.getElementById("myImage");
+    let availableSrc = 'static/' + image.src.split('static/')[1]
 
     image.classList.add("hidden");
     setTimeout(function() {
@@ -17,4 +17,24 @@ function changeImage() {
         }
         image.classList.remove("hidden");
     }, 300)
+}
+
+let rotateDeg = 180
+function changeSelector() {
+    let selector = document.getElementById('selector_city');
+    arrows = [
+        document.getElementById('arrow1').style, 
+        document.getElementById('arrow2').style
+    ]
+    
+    if (rotateDeg === 0){
+        rotateDeg = 180
+        selector.style.display = 'none'
+    }else{
+        rotateDeg = 0
+        selector.style.display = 'flex'
+    }
+    arrows.forEach(arrow => {
+        arrow.transform = 'rotate(' + rotateDeg + 'deg)';
+    });
 }
