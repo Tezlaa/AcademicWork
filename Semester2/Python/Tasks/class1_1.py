529da2ff24745d825c729a55324dbed1ab05e010
Tezla <Bogdantihenko60@gmail.com>
2023-05-27 15:24:09 +0300

    fix lab work, add math.gcd and add minimum docsting

--- Semester2/Python/Tasks/class1_1.py
+++ Semester2/Python/Tasks/class1_1.py
@@ -1,9 +1,13 @@
+from math import gcd
+
+
 def valid_class_receipt(func):
-    def wrapper(*args, **kwarks):
+    """Decorator to validate the second argument"""
+    def wrapper(*args, **kwargs):
         class_ = args[1]
         if not isinstance(class_, SimpleFraction):
             raise TypeError(f'{type(class_)} is not the class {SimpleFraction}')
-        return func(*args, **kwarks)
+        return func(*args, **kwargs)
     return wrapper
 
 
@@ -14,37 +18,56 @@ class SimpleFraction:
     
     @valid_class_receipt
     def __add__(self, other):
+        """Addition operator overload."""
         ceiling = (self.number_ceiling * other.number_floor) + (other.number_ceiling * self.number_floor)
         floor = self.number_floor * other.number_floor
-        return SimpleFraction(ceiling, floor)
+        result = SimpleFraction(ceiling, floor)
+        result.simplify()
+        return result
     
     @valid_class_receipt
     def __sub__(self, other):
+        """Subtraction operator overload."""
         ceiling = (self.number_ceiling * other.number_floor) - (other.number_ceiling * self.number_floor)
         floor = self.number_floor * other.number_floor
-        return SimpleFraction(ceiling, floor)
+        result = SimpleFraction(ceiling, floor)
+        result.simplify()
+        return result
     
     @valid_class_receipt
     def __mul__(self, other):
+        """Multiplication operator overload."""
         ceiling = self.number_ceiling * other.number_ceiling
         floor = self.number_floor * other.number_floor
-        return SimpleFraction(ceiling, floor)
+        result = SimpleFraction(ceiling, floor)
+        result.simplify()
+        return result
     
     @valid_class_receipt
     def __truediv__(self, other):
+        """Division operator overload."""
         ceiling = self.number_ceiling * other.number_floor
         floor = self.number_floor * other.number_ceiling
-        return SimpleFraction(ceiling, floor)
+        result = SimpleFraction(ceiling, floor)
+        result.simplify()
+        return result
+    
+    def simplify(self):
+        """Simplify the fraction to its minimal expression."""
+        common_divisor = gcd(self.number_ceiling, self.number_floor)
+        self.number_ceiling //= common_divisor
+        self.number_floor //= common_divisor
     
     def __repr__(self) -> str:
+        """Representation of the fraction."""
         return f'{self.number_ceiling}/{self.number_floor}'
-    
-    
+
+
 if __name__ == "__main__":
     fraction1 = SimpleFraction(5, 1)
     fraction2 = SimpleFraction(4, 10)
     
-    result = fraction1 + 1
+    result = fraction1 + fraction2
     print(result)
 
     result = fraction1 - fraction2
